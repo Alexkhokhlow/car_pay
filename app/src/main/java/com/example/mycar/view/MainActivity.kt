62847199c0af691d362abb0f8f7d57fc1b22a5ab
Alexey KHokhlow <lomtik25@gmail.com>
2021-01-30 12:47:09 +0300

    First norm commit

--- app/src/main/java/com/example/mycar/view/MainActivity.kt
+++ app/src/main/java/com/example/mycar/view/MainActivity.kt
@@ -11,19 +11,12 @@ import com.example.mycar.databinding.FragmentMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var reciver:AirMod
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        reciver = AirMod()
-
-        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
-            registerReceiver(reciver,it)
-        }
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
@@ -40,8 +33,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(reciver)
-    }
 }
